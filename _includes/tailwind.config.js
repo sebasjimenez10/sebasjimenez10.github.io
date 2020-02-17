a9c6dfd062bedfeccc341acfae58950ed3d539ad
Sebastian Jimenez V <sebasjimenezv@gmail.com>
2020-02-17 17:35:10 -0500

    Finished landing

---
+++ _includes/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Roboto-Thin', 'sans-serif'],
+      body: ['Roboto-Thin', 'sans-serif']
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
