37eea727b1fd321850b44affc579447b65c371ff
Sebastian Jimenez V <sebasjimenezv@gmail.com>
2020-02-19 10:26:53 -0500

    Improvements

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   theme: {
     fontFamily: {
-      display: ['Roboto-Thin', 'Roboto-Regular', 'Roboto-Light', 'Roboto-Bold', 'sans-serif'],
-      body: ['Roboto-Thin', 'Roboto-Regular', 'Roboto-Light', 'Roboto-Bold', 'sans-serif']
+      display: ['Roboto', 'sans-serif'],
+      body: ['Roboto', 'sans-serif']
     },
     screens: {
       'sm': '640px',
